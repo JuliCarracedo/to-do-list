fc5f93e946a3e30ac305d49842eb868ed0ffa1b5
JuliCarracedo <julicarra@hotmail.com>
2021-08-17 21:29:45 -0300

    Add HtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
-function component() {
-    const element = document.createElement('div');
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+   return element;
+ }
+
+ document.body.appendChild(component());
